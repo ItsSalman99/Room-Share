def7bb68e9ce461bc1ce8cbaaea8dae118c3b17e
Salman Abbas <salmanabbas985@gmail.com>
2021-12-17 21:24:18 +0300

    navigation.blade.php updated

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <img src="/assets/imgs/logo.png" class="w-8 mx-auto h-8 fill-current text-gray-500" />
                     </a>
                 </div>
 
