7d0c2da08f77fbe7d3cb475c3b3afe9c8ca30e11
Salman Abbas <salmanabbas985@gmail.com>
2021-12-18 12:14:34 +0300

    HomeController updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('pages.index');
+        $rooms = Room::inRandomOrder()->limit(9)->get();
+        return view('pages.index')->with(['rooms' =>$rooms]);
     }
 }
