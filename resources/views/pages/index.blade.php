d0ca40cac9cb2facab36e9a8bffd5781c87578e4
Salman Abbas <salmanabbas985@gmail.com>
2021-12-18 12:15:42 +0300

    page/index index.blade.php updated

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,7 +7,7 @@
       <h2 class="text-2xl font-extrabold tracking-tight text-gray-900">Accomodations you may like!</h2>
   
       <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-        @foreach([1,2,3,4,5,6,7,8] as $items)
+        @foreach($rooms as $items)
         <div class="group relative">
           <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
             <img src="{{asset('assets/imgs/room.jpg')}}" class="w-full h-full object-center object-cover lg:w-full lg:h-full">
@@ -17,11 +17,11 @@
               <h3 class="text-sm text-gray-700">
                 <a href="#" class="font-extrabold">
                   <span aria-hidden="true" class="absolute inset-0"></span>
-                  {{$items}}
+                  {{$items->room_name}}
                 </a>
               </h3>
             </div>
-            <p class="text-sm font-medium text-gray-900">Pkr. {{$items}}</p>
+            <p class="text-sm font-medium text-gray-900">Pkr. {{$items->price}}</p>
           </div>
         </div>
         @endforeach
