3d91cc5abcc89afa1e4d02a733ebbd014a0fc0f0
muhammadali10225 <muhammadali000617@gmail.com>
2021-12-19 17:28:07 +0500

    Post Method Added for saving rooms data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -26,4 +26,41 @@ class DashboardController extends Controller
     {
         return view('dashboard.addrooms');
     }
+
+    public function saverooms(Request $request)
+    {
+        $request->validate([
+            'room_name' => 'string|required',
+            'room_type' => 'string|required',
+            'room_img' => 'image|mimes:jpeg,jpg',
+            'total_occupancy' => 'integer|required',
+            'total_bedrooms' => 'required',
+            'total_bathrooms' => 'required',
+            'room_summary' => 'required',
+            'address' => 'required',
+            'has_tv' => 'required',
+            'has_kitchen' => 'required',
+            'has_aircondition' => 'required',
+            'has_heating' => 'required',
+            'has_internet' => 'required',
+            'price' => 'required',
+
+        ]);
+        dd($request);
+
+        // Room::create([
+        //     'room_name' => $request->room_name,
+        //     'home_type' => $request->home_type,
+        //     'room_img' => $request->room_img,
+        //     'total_occupancy' => $request->total_occupancy,
+        //     'total_bedrooms' => $request->total_bedrooms,
+        //     'total_bathrooms' => $request->total_bathrooms,
+        //     'room_summary' => $request->room_summary,
+        //     'address' => $request->address,
+        //     'has_tv' => $request->has_tv,
+        //     'has_kitchen' => $request->has_kitchen,
+        //     'has_aircondition' => $request->has_aircondition,
+        //     'has_heating' => $request->has_heating,
+        //     'has_internet' => $request->has_internet,
+    }
 }
