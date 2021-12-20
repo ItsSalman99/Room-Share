9b2508fb0c6ea893627cc9ad184502417b3766bc
Salman Abbas <salmanabbas985@gmail.com>
2021-12-20 21:20:46 +0300

    Create rooms functionality done in RoomsController.php

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -2,7 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
 class RoomsController extends Controller
 {
+    public function Create(Request $request)
+    {
+        //Validation input requests
+        $request->validate([
+            'room_name' => 'string|required',
+            'room_type' => 'string|required',
+            'room_img' => 'image|mimes:jpeg,jpg',
+            'total_occupancy' => 'integer|required',
+            'total_bedrooms' => 'required',
+            'total_bathrooms' => 'required',
+            'room_summary' => 'required',
+            'address' => 'required',
+            'has_tv' => 'required',
+            'has_kitchen' => 'required',
+            'has_aircondition' => 'required',
+            'has_heating' => 'required',
+            'has_internet' => 'required',
+            'price' => 'required',
 
+        ]);
+
+        //Checking & Storing file in server
+        if ($request->hasFile('room_img')) {
+            $filename = $request->room_img->getClientOriginalName();
+
+            $request->room_img->storeAs('rooms', $filename, 'public');
+            
+
+        }
+
+        //dd($request);
+
+        //Creating Room Data
+        Room::create([
+            'room_name' => $request->room_name,
+            'home_type' => $request->room_type,
+            'img' => $filename,
+            'total_occupancy' => $request->total_occupancy,
+            'total_bedrooms' => $request->total_bedrooms,
+            'total_bathrooms' => $request->total_bathrooms,
+            'summary' => $request->room_summary,
+            'address' => $request->address,
+            'has_tv' => $request->has_tv,
+            'has_kitchen' => $request->has_kitchen,
+            'has_air_con' => $request->has_aircondition,
+            'has_heating' => $request->has_heating,
+            'has_internet' => $request->has_internet,
+            'price' => $request->price,
+        ]);
+
+        return redirect()->route('addrooms')->withMessage('Room has been added successfully!');
+
+    }
 }
