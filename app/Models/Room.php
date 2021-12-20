2d0f180ea6bb06a34d0b798b17761c8e73c10f72
Salman Abbas <salmanabbas985@gmail.com>
2021-12-20 21:21:11 +0300

    $fillable added in Room.php model

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'room_name',
+        'home_type',
+        'img',
+        'total_occupancy',
+        'total_bedrooms',
+        'total_bathrooms',
+        'summary',
+        'address',
+        'has_tv',
+        'has_kitchen',
+        'has_air_con',
+        'has_heating',
+        'has_internet',
+        'price',
+        'owner_id',
+        'created_at',
+        'updated_at'
+    ];
+
 }
