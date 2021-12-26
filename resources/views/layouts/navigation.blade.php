10e7779431d399c1a0eabc0981357f0ced683066
Salman Abbas <salmanabbas985@gmail.com>
2021-12-26 18:32:03 +0300

    updated

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,11 @@
                         {{ __('Rooms') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('rooms.index')" :active="request()->routeIs('dashboard')">
+                        {{ __('Reservations') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
