21481cd77796350f882496ce4756d9fc0505b339
Salman Abbas <salmanabbas985@gmail.com>
2021-12-30 21:13:19 +0300

    Update rooms added

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -18,6 +18,78 @@ class RoomsController extends Controller
 
     }
 
+    public function edit($id)
+    {
+        $room = Room::findOrFail($id);
+
+        return view('dashboard.host.edit')->with(['room' => $room]);
+
+    }
+
+    public function update($id,Request $request)
+    {
+        
+        $room = Room::findOrFail($id);
+        
+        //Validation input requests
+        $request->validate([
+            'room_name' => 'string|required',
+            'room_type' => 'string|required',
+            'room_img' => 'image|mimes:jpeg,jpg',
+            'total_occupancy' => 'integer|required',
+            'total_bedrooms' => 'required',
+            'total_bathrooms' => 'required',
+            'room_summary' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'has_tv' => 'required',
+            'has_kitchen' => 'required',
+            'has_aircondition' => 'required',
+            'has_heating' => 'required',
+            'has_internet' => 'required',
+            'price' => 'required',
+
+        ]);
+
+        //dd($request);
+         
+        //Checking & Storing file in server
+        if ($request->hasFile('room_img')) {
+            $filename = $request->room_img->getClientOriginalName();
+            if ($room->room_img) {
+                Storage::delete(['/public/rooms/'. $room->room_img]);
+            }
+            $request->room_img->storeAs('rooms', $filename, 'public');
+        }
+        else{
+            $filename = $room->img;
+        }
+
+        //Updating Room Data
+        $room->room_name = $request->room_name;
+        $room->home_type = $request->room_type;
+        $room->img = $filename;
+        $room->total_occupancy = $request->total_occupancy;
+        $room->total_bedrooms = $request->total_bedrooms;
+        $room->total_bathrooms = $request->total_bathrooms;
+        $room->summary = $request->room_summary;
+        $room->description = $request->room_description;
+        $room->city = $request->city;
+        $room->address = $request->address;
+        $room->has_tv = $request->has_tv;
+        $room->has_kitchen = $request->has_kitchen;
+        $room->has_air_con = $request->has_aircondition;
+        $room->has_heating = $request->has_heating;
+        $room->has_internet = $request->has_internet;
+        $room->price = $request->price;
+        $room->owner_id = Auth::user()->id;
+
+        $room->save();
+
+        return redirect()->route('editroom',$room->id)->with(['room'=> $room])->withMessage('Room has been updated successfully!');
+    }
+
+
     public function Create(Request $request)
     {
         //Validation input requests
