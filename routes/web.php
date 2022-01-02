7d5b908cb93cf793bc991784a4ab7b0a16970fc1
Salman Abbas <salmanabbas985@gmail.com>
2022-01-02 16:18:03 +0300

    remove verified for instance

--- routes/web.php
+++ routes/web.php
@@ -33,14 +33,14 @@ Route::get('/checkout-pay', [CheckoutController::class, 'payview'])->middleware(
 Route::post('/pay-out', [CheckoutController::class, 'confirmPay'])->middleware(['auth'])->name('payout');
 
 //Admin Routes
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth','verified'])->name('dashboard');
-Route::get('/dashboard/rooms', [DashboardController::class, 'rooms'])->middleware(['auth','verified'])->name('rooms.index');
-Route::get('/Add-Rooms', [DashboardController::class, 'addrooms'])->middleware(['auth','verified'])->name('addrooms');
-Route::post('/Add-Rooms', [RoomsController::class, 'Create'])->middleware(['auth','verified'])->name('createRooms');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard/rooms', [DashboardController::class, 'rooms'])->middleware(['auth'])->name('rooms.index');
+Route::get('/Add-Rooms', [DashboardController::class, 'addrooms'])->middleware(['auth'])->name('addrooms');
+Route::post('/Add-Rooms', [RoomsController::class, 'Create'])->middleware(['auth'])->name('createRooms');
 Route::post('/deleteroom/{id}', [RoomsController::class,'deleteroom'])->name('delroom');
 Route::get('my-reservations', [ReservationsController::class,'myreservations'])->middleware(['auth'])->name('user-reservations');
-Route::get('edit-room/{id}', [RoomsController::class,'edit'])->middleware(['auth','verified'])->name('editroom');
-Route::post('/updateroom/{id}', [RoomsController::class,'update'])->middleware(['auth','verified'])->name('updateroom');
+Route::get('edit-room/{id}', [RoomsController::class,'edit'])->middleware(['auth'])->name('editroom');
+Route::post('/updateroom/{id}', [RoomsController::class,'update'])->middleware(['auth'])->name('updateroom');
 
 //Customer Routes
 Route::post('/storecustomer', [CustomerController::class, 'store'])->name('storeCustomer');
