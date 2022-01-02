491f313eb2c422a68eb2ae943334a3e524a48fb6
Salman Abbas <salmanabbas985@gmail.com>
2022-01-02 16:26:40 +0300

    heroku error on event (comment)

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -47,7 +47,7 @@ class RegisteredUserController extends Controller
 
         $user->attachRole('Host');
 
-        event(new Registered($user));
+        //event(new Registered($user));
 
         Auth::login($user);
 
